21c5fd1410f34fc92d78e6969ff3044ba5899074
Nicolas VINCENT <nicolas.vincent@vossloh.com>
2021-08-30 14:02:43 +0200

    Move SparkplugTopic to its own file
    Signed-off-by: Nicolas VINCENT <nicolas.vincent@vossloh.com>

--- enki.py
+++ enki.py
@@ -16,11 +16,11 @@ import paho.mqtt.client as mqtt
 sys.path.insert(0, "tahu/client_libraries/python/")
 import sparkplug_b as sparkplug
 from sparkplug_b import *
+from sp_topic import SparkplugTopic
 
 # Application Variables
 myUsername = "admin"
 myPassword = "changeme"
-sp_namespace = "spBv1.0"
 
 int_value_types = [MetricDataType.Int8,
                    MetricDataType.Int16,
@@ -94,52 +94,6 @@ def datatype_to_str(datatype):
         return "Template"
     else:
         return "Invalid datatype %s" % str(datatype)
-
-
-class SparkplugTopic:
-
-    @staticmethod
-    def create(group_id, cmd, eon_id, dev_id=None):
-        topic_str = "%s/%s/%s/%s" % (sp_namespace, group_id, cmd, eon_id)
-        if dev_id is not None:
-            topic_str += "/%s" % (dev_id)
-        return SparkplugTopic(topic_str)
-
-    def __init__(self, topic):
-        tokens = topic.split("/")
-        self.topic = topic
-        if len(tokens) != 4 and len(tokens) != 5:
-            self.valid_topic = False
-        else:
-            self.namespace = tokens[0]
-            self.group_id = tokens[1]
-            self.message_type = tokens[2]
-            self.edge_node_id = tokens[3]
-            if len(tokens) == 5:
-                self.device_id = tokens[4]
-            else:
-                self.device_id = None
-
-    def is_nbirth(self):
-        return self.message_type == "NBIRTH"
-
-    def is_dbirth(self):
-        return self.message_type == "DBIRTH"
-
-    def is_ndata(self):
-        return self.message_type == "NDATA"
-
-    def is_ddata(self):
-        return self.message_type == "DDATA"
-
-    def eon_id(self):
-        return self.edge_node_id
-
-    def __repr__(self):
-        return "%s(\"%s\")" % (type(self).__name__, self.topic)
-
-    def __str__(self):
-        return "%s" % (self.topic)
 
 
 def add_value_to_element(element, value, type):
---
+++ sp_topic.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+"""Sparkplug Topic management tools."""
+
+SP_NAMESPACE = "spBv1.0"
+
+
+class SparkplugTopic:
+    """Handles a Sparkplug Topic."""
+
+    @staticmethod
+    def create(group_id, cmd, eon_id, dev_id=None):
+        """Create Sparkplug Topic from parameters."""
+        topic_str = "%s/%s/%s/%s" % (SP_NAMESPACE, group_id, cmd, eon_id)
+        if dev_id is not None:
+            topic_str += "/%s" % (dev_id)
+        return SparkplugTopic(topic_str)
+
+    def __init__(self, topic):
+        tokens = topic.split("/")
+        self.topic = topic
+        if len(tokens) != 4 and len(tokens) != 5:
+            self.valid_topic = False
+        else:
+            self.namespace = tokens[0]
+            self.group_id = tokens[1]
+            self.message_type = tokens[2]
+            self.edge_node_id = tokens[3]
+            if len(tokens) == 5:
+                self.device_id = tokens[4]
+            else:
+                self.device_id = None
+
+    def is_nbirth(self):
+        """True for node birth topics."""
+        return self.message_type == "NBIRTH"
+
+    def is_dbirth(self):
+        """True for device birth topics."""
+        return self.message_type == "DBIRTH"
+
+    def is_ndata(self):
+        """True for node data topics."""
+        return self.message_type == "NDATA"
+
+    def is_ddata(self):
+        """True for device data topics."""
+        return self.message_type == "DDATA"
+
+    def eon_id(self):
+        """Return Edge of Network Node ID."""
+        return self.edge_node_id
+
+    def __repr__(self):
+        return "%s(\"%s\")" % (type(self).__name__, self.topic)
+
+    def __str__(self):
+        return "%s" % (self.topic)
